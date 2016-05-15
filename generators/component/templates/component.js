05a5ff889010a46bbcab7b0b0c4a226e1b639238
Artem Lipatov <a.lipatov@2gis.ru>
2016-05-15 12:56:25 +0300

    generate let instead of var

--- generators/component/templates/component.js
+++ generators/component/templates/component.js
@@ -1,5 +1,5 @@
-<% if (isCss) { %>var css = require('./<%= name %>.css');
-var { extend } = require('catbee-utils');<% } %>
+<% if (isCss) { %>let css = require('./<%= name %>.css');
+let { extend } = require('catbee-utils');<% } %>
 
 class <%= className %> {
   <% if (isCss) { %>@extend({ css })<% } %>
