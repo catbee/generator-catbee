c509a51e262ef5904a2430883a38fe2cb000c373
Artem Lipatov <a.lipatov@2gis.ru>
2016-05-15 12:59:41 +0300

    use destructuring when require catbee-utils

--- generators/component/templates/component.js
+++ generators/component/templates/component.js
@@ -1,5 +1,5 @@
 <% if (isCss) { %>var css = require('./<%= name %>.css');
-var extend = require('catbee-utils').extend;<% } %>
+var { extend } = require('catbee-utils');<% } %>
 
 class <%= className %> {
   <% if (isCss) { %>@extend({ css })<% } %>
